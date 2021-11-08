a57c211d4a01aa0e2d069e8b0ea94e46e039fb36
Rfrias <rfrias@ccmsi.com>
2021-11-08 17:21:57 -0300

    Commint changes for Presentation Course Controller, Views, Stying, Authentication

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -32,7 +32,7 @@ namespace CleanArch.Mvc
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("UniversityIdentityDBConnection")));
-            
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
